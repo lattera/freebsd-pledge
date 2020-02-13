60c1512bbc92749088bc19174574c9ad80b1dc27
Mathieu <sigsys@gmail.com>
2020-02-13 02:15:38 -0500

    Add some very basic plain program tests

---
+++ tests/sys/pledge/pledge_super_basic_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+
+int main() {
+	int r;
+	r = pledge("stdio", "");
+	if (r < 0)
+		err(1, "pledge");
+	r = write(STDOUT_FILENO, "ok!\n", 4);
+	if (r < 0)
+		err(1, "write");
+	_exit(0);
+	err(1, "_exit");
+	return 0;
+}
