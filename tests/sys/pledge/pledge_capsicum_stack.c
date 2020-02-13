b6f51c80d6069b4317b5836d882cd8155e94d8e0
Mathieu <sigsys@gmail.com>
2020-02-13 02:56:47 -0500

    Add simple test of Capsicum and pledge(2) restrictions stacking

---
+++ tests/sys/pledge/pledge_capsicum_stack.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/capsicum.h>
+
+int main() {
+	int r;
+	r = pledge("error stdio flock", "");
+	if (r < 0)
+		err(1, "pledge");
+	/* this is allowed by pledge("stdio") but not Capsicum */
+	r = access("/etc/malloc.conf", R_OK);
+	if (r < 0 && errno != ENOENT)
+		err(1, "access");
+	r = cap_enter();
+	if (r < 0)
+		err(1, "cap_enter");
+	r = write(STDOUT_FILENO, "ok!\n", 4);
+	if (r < 0)
+		err(1, "write");
+	r = access("/etc/malloc.conf", R_OK);
+	if (!(r < 0 && errno == ECAPMODE))
+		errx(1, "access() shouldn't have worked!");
+	/* flock would be allowed by Capsicum but not pledge("stdio") */
+	r = flock(STDIN_FILENO, LOCK_SH);
+	if (r < 0)
+		err(1, "flock");
+	r = pledge("error stdio", "");
+	if (r < 0)
+		err(1, "pledge");
+	r = flock(STDIN_FILENO, LOCK_UN);
+	if (!(r < 0 && errno == ECAPMODE))
+		errx(1, "flock() shouldn't have worked!");
+	return 0;
+}
