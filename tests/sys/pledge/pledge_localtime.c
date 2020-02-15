e63ec00d46070f2acfaf7a9be217b06bc22e9bb4
Mathieu <sigsys@gmail.com>
2020-02-15 12:35:37 -0500

    Add localtime() test.

---
+++ tests/sys/pledge/pledge_localtime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+#include <time.h>
+
+int main() {
+	int r;
+	r = pledge("stdio", "");
+	if (r < 0)
+		err(1, "pledge");
+	time_t now;
+	struct tm *loc;
+	time(&now);
+	loc = localtime(&now);
+	if (!loc)
+		err(1, "localtime");
+	return 0;
+}
