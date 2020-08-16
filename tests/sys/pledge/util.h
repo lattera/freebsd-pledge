e6acdeb16dac020dfee78af46b7d5decd8352fe9
Mathieu <sigsys@gmail.com>
2020-08-16 16:29:09 -0400

    Add test for "prot_exec" promise.

--- tests/sys/pledge/util.h
+++ tests/sys/pledge/util.h
@@ -13,3 +13,31 @@
 
 #define EXPECT(expr) TRY(expr, true)
 #define REJECT(expr) TRY(expr, false)
+
+
+#define TRY_BOOL(expr, should_work) do { \
+	if (!(expr)) { \
+		if (should_work) \
+			err(1, "%s", #expr); \
+	} else { \
+		if (!should_work) \
+			errx(1, "%s: %s", #expr, "shouldn't have worked"); \
+	} \
+} while (0)
+
+#define EXPECT_BOOL(expr) TRY_BOOL(expr, true)
+#define REJECT_BOOL(expr) TRY_BOOL(expr, false)
+
+
+#define TRY_PTR(expr, should_work) do { \
+	if ((expr) == NULL) { \
+		if (should_work) \
+			err(1, "%s", #expr); \
+	} else { \
+		if (!should_work) \
+			errx(1, "%s: %s", #expr, "shouldn't have worked"); \
+	} \
+} while (0)
+
+#define EXPECT_PTR(expr) TRY_PTR(expr, true)
+#define REJECT_PTR(expr) TRY_PTR(expr, false)
