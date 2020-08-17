ca7671c0c07b98709ac36e37eb3825d0ef01b263
Mathieu <sigsys@gmail.com>
2020-08-17 00:17:51 -0400

    Add test.

---
+++ tests/sys/pledge/sysfilctl_path_open.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include <sys/sysfil.h>
+
+#include "util.h"
+
+static void
+test_rpath(bool should_work)
+{
+	int fd;
+	TRY((fd = open("/", O_RDONLY)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+	TRY((fd = open("/dev", O_RDONLY)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+	TRY((fd = open("/dev/zero", O_RDONLY)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+	TRY((fd = open("/dev/null", O_RDONLY)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+}
+
+static void
+test_wpath(bool should_work)
+{
+	int fd;
+	TRY((fd = open("/dev/null", O_WRONLY)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+}
+
+static void
+test_cpath(bool should_work)
+{
+	int fd;
+	TRY((fd = open("/dev/null", O_WRONLY|O_CREAT)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+}
+
+static void
+test_xpath(bool should_work)
+{
+	int fd;
+	TRY((fd = open("/bin/sh", O_EXEC)), should_work);
+	if (should_work)
+		EXPECT(close(fd));
+}
+
+#define	DO_SYSFILCTL(...) \
+({ \
+	int sysfils[] = { SYSFIL_ERROR, __VA_ARGS__ }; \
+	sysfilctl(SYSFILCTL_MASK, sysfils, nitems(sysfils)); \
+})
+
+int
+main()
+{
+	EXPECT(DO_SYSFILCTL(SYSFIL_STDIO, SYSFIL_PATH, SYSFIL_RPATH, SYSFIL_WPATH, SYSFIL_CPATH, SYSFIL_EXEC));
+	test_rpath(true);
+	test_wpath(true);
+	test_cpath(true);
+	test_xpath(true);
+	EXPECT(DO_SYSFILCTL(SYSFIL_STDIO, SYSFIL_PATH, SYSFIL_RPATH, SYSFIL_WPATH, SYSFIL_EXEC));
+	test_rpath(true);
+	test_wpath(true);
+	test_cpath(false);
+	test_xpath(true);
+	EXPECT(DO_SYSFILCTL(SYSFIL_STDIO, SYSFIL_PATH, SYSFIL_RPATH, SYSFIL_EXEC));
+	test_rpath(true);
+	test_wpath(false);
+	test_cpath(false);
+	test_xpath(true);
+	EXPECT(DO_SYSFILCTL(SYSFIL_STDIO, SYSFIL_PATH, SYSFIL_EXEC));
+	test_rpath(false);
+	test_wpath(false);
+	test_cpath(false);
+	test_xpath(true);
+	EXPECT(DO_SYSFILCTL(SYSFIL_STDIO, SYSFIL_PATH));
+	test_rpath(false);
+	test_wpath(false);
+	test_cpath(false);
+	test_xpath(false);
+}
