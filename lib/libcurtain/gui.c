eb911dc16602ee4b01a2171294697c7f19991aa0
Mathieu <sigsys@gmail.com>
2021-07-08 03:28:37 -0400

    Cleanup X11/wayland sockets handling

--- lib/libcurtain/gui.c
+++ lib/libcurtain/gui.c
@@ -9,6 +9,18 @@
 #include <sysexits.h>
 #include <unistd.h>
 
+static struct curtain_slot *
+get_slot(struct curtain_config *cfg)
+{
+	struct curtain_slot *slot;
+	if (cfg->on_exec)
+		curtain_enable((slot = curtain_slot_neutral()), CURTAIN_ON_EXEC);
+	else
+		slot = curtain_slot();
+	return (slot);
+}
+
+
 static char *tmp_xauth_file = NULL;
 static char *display_unix_socket = NULL;
 
@@ -30,19 +42,66 @@ cleanup_x11(void)
 }
 
 static void
-prepare_x11(struct curtain_slot *slot, bool trusted)
+prepare_x11(struct curtain_config *cfg)
 {
+	struct curtain_slot *slot;
 	int r;
 	char *p, *display;
 	pid_t pid;
 	int status;
 
-	p = getenv("DISPLAY");
-	if (!p || !*p) {
+	display = getenv("DISPLAY");
+	if (!display || !*display) {
 		warnx("DISPLAY environment variable not set");
 		return;
 	}
-	display = p;
+
+	p = getenv("TMPDIR");
+	r = asprintf(&p, "%s/%s.xauth.XXXXXXXXXXXX",
+	    p && *p ? p : "/tmp", getprogname());
+	if (r < 0)
+		err(EX_TEMPFAIL, "asprintf");
+	r = mkstemp(p);
+	if (r < 0) {
+		free(p);
+		warn("mkstemp");
+		return;
+	}
+	r = close(r);
+	if (r < 0)
+		warn("%s", p);
+	tmp_xauth_file = p;
+	atexit(cleanup_x11);
+
+	pid = vfork();
+	if (pid < 0) {
+		warn("fork");
+		return;
+	} else if (pid == 0) {
+		err_set_exit(_exit);
+		if (cfg->x11_trusted)
+			execlp("xauth", "xauth",
+			    "extract", tmp_xauth_file, display, NULL);
+		else
+			execlp("xauth", "xauth", "-f", tmp_xauth_file,
+			    "generate", display, ".", "untrusted",
+			    "timeout", "0", NULL);
+		err(EX_OSERR, "xauth");
+	}
+	err_set_exit(NULL);
+
+	r = waitpid(pid, &status, 0);
+	if (r < 0) {
+		warn("waitpid");
+		return;
+	} else if (!(WIFEXITED(status) && WEXITSTATUS(status) == 0)) {
+		if (WIFSIGNALED(status))
+			warnx("xauth terminated with signal %d", WTERMSIG(status));
+		else
+			warnx("xauth exited with code %d", WEXITSTATUS(status));
+		return;
+	}
+
 	if (display[0] == ':')
 		p = display + 1;
 	else if (strncmp(display, "unix:", 5) == 0)
@@ -56,71 +115,29 @@ prepare_x11(struct curtain_slot *slot, bool trusted)
 			err(EX_TEMPFAIL, "asprintf");
 	}
 
-	p = getenv("TMPDIR");
-	r = asprintf(&p, "%s/%s.xauth.XXXXXXXXXXXX",
-	    p && *p ? p : "/tmp", getprogname());
-	if (r < 0)
-		err(EX_TEMPFAIL, "asprintf");
-	r = mkstemp(p);
-	if (r < 0)
-		err(EX_OSERR, "mkstemp");
-	r = close(r);
-	if (r < 0)
-		warn("%s", p);
-	tmp_xauth_file = p;
-	atexit(cleanup_x11);
-
-	pid = vfork();
-	if (pid < 0)
-		err(EX_TEMPFAIL, "fork");
-	if (pid == 0) {
-		err_set_exit(_exit);
-		if (trusted)
-			execlp("xauth", "xauth",
-			    "extract", tmp_xauth_file, display, NULL);
-		else
-			execlp("xauth", "xauth", "-f", tmp_xauth_file,
-			    "generate", display, ".", "untrusted",
-			    "timeout", "0", NULL);
-		err(EX_OSERR, "xauth");
-	}
-	err_set_exit(NULL);
-
-	r = waitpid(pid, &status, 0);
-	if (r < 0)
-		err(EX_OSERR, "waitpid");
-	if (!(WIFEXITED(status) && WEXITSTATUS(status) == 0)) {
-		if (WIFSIGNALED(status))
-			errx(EX_UNAVAILABLE, "xauth terminated with signal %d",
-			    WTERMSIG(status));
-		errx(EX_UNAVAILABLE, "xauth exited with code %d", WEXITSTATUS(status));
-	}
-
 	r = setenv("XAUTHORITY", tmp_xauth_file, 1);
 	if (r < 0)
 		err(EX_TEMPFAIL, "setenv");
 
-	if (display_unix_socket) {
-		r = curtain_unveil(slot, display_unix_socket,
+	slot = get_slot(cfg);
+
+	if (display_unix_socket)
+		curtain_unveil(slot, display_unix_socket,
 		    CURTAIN_UNVEIL_INSPECT, UPERM_CONNECT|UPERM_INSPECT);
-		if (r < 0)
-			err(EX_OSERR, "%s", display_unix_socket);
-	}
-	if (tmp_xauth_file) {
-		r = curtain_unveil(slot, tmp_xauth_file,
+	if (tmp_xauth_file)
+		curtain_unveil(slot, tmp_xauth_file,
 		    CURTAIN_UNVEIL_INSPECT, UPERM_READ);
-		if (r < 0)
-			err(EX_OSERR, "%s", tmp_xauth_file);
-	}
 }
 
 
 static void
-prepare_wayland(struct curtain_slot *slot)
+prepare_wayland(struct curtain_config *cfg)
 {
+	struct curtain_slot *slot;
 	const char *display;
 	char *socket;
 	int r;
+	slot = get_slot(cfg);
 	display = getenv("WAYLAND_DISPLAY");
 	if (!display)
 		display = "wayland-0";
@@ -139,31 +156,18 @@ prepare_wayland(struct curtain_slot *slot)
 		if (r < 0)
 			err(EX_TEMPFAIL, "asprintf");
 	}
-	r = curtain_unveil(slot, socket,
+	curtain_unveil(slot, socket,
 	    CURTAIN_UNVEIL_INSPECT, UPERM_CONNECT|UPERM_INSPECT);
-	if (r < 0)
-		err(EX_OSERR, "%s", socket);
 	free(socket);
 }
 
 
-static struct curtain_slot *
-get_slot(struct curtain_config *cfg)
-{
-	struct curtain_slot *slot;
-	if (cfg->on_exec)
-		curtain_enable((slot = curtain_slot_neutral()), CURTAIN_ON_EXEC);
-	else
-		slot = curtain_slot();
-	return (slot);
-}
-
 int
 curtain_config_gui(struct curtain_config *cfg)
 {
 	if (cfg->x11 || cfg->x11_trusted)
-		prepare_x11(get_slot(cfg), cfg->x11_trusted);
+		prepare_x11(cfg);
 	if (cfg->wayland)
-		prepare_wayland(get_slot(cfg));
+		prepare_wayland(cfg);
 	return (0);
 }
