daf94cadc44db6bb457feecd58da115ebdb4dce3
Mathieu <sigsys@gmail.com>
2021-12-08 04:31:26 -0500

    curtain: Always allow vm.overcommit sysctl
    It's used by jemalloc(3).

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -169,9 +169,9 @@ SYSCTL_PROC(_vm, OID_AUTO, swap_total, CTLTYPE_U64 | CTLFLAG_RD | CTLFLAG_MPSAFE
     "Total amount of available swap storage.");
 
 static int overcommit = 0;
-SYSCTL_INT(_vm, VM_OVERCOMMIT, overcommit, CTLFLAG_RW, &overcommit, 0,
-    "Configure virtual memory overcommit behavior. See tuning(7) "
-    "for details.");
+SYSCTL_INT(_vm, VM_OVERCOMMIT, overcommit, CTLFLAG_RW | CTLFLAG_RESTRICT,
+    &overcommit, 0,
+    "Configure virtual memory overcommit behavior. See tuning(7) for details.");
 static unsigned long swzone;
 SYSCTL_ULONG(_vm, OID_AUTO, swzone, CTLFLAG_RD, &swzone, 0,
     "Actual size of swap metadata zone");
